ea8d0421a4a9d9a2a095d103429e72eae420b333
Loeli Cab <loeliacab92@gmail.com>
2026-05-31 20:43:27 -0500

    Added local storage for  the recipes

--- src/stores/recipes.js
+++ src/stores/recipes.js
@@ -1,27 +1,31 @@
 import { defineStore } from 'pinia'
 
+const savedRecipes = localStorage.getItem('recipes')
+
 export const useRecipesStore = defineStore('recipes', {
   state: () => ({
-    recipes: [
-      {
-        id: 1,
-        title: 'Chicken Alfredo',
-        ingredients: 'Chicken, Pasta, Cream',
-        instructions: 'Cook pasta, cook chicken, then mix together.',
-      },
-      {
-        id: 2,
-        title: 'Pancakes',
-        ingredients: 'Flour, Milk, Eggs',
-        instructions: 'Mix ingredients and cook on a pan.',
-      },
-      {
-        id: 3,
-        title: 'Chocolate Cake',
-        ingredients: 'Flour, Cocoa Powder, Sugar',
-        instructions: 'Mix ingredients and bake.',
-      },
-    ],
+    recipes: savedRecipes
+      ? JSON.parse(savedRecipes)
+      : [
+          {
+            id: 1,
+            title: 'Chicken Alfredo',
+            ingredients: 'Chicken, Pasta, Cream',
+            instructions: 'Cook pasta, cook chicken, then mix together.',
+          },
+          {
+            id: 2,
+            title: 'Pancakes',
+            ingredients: 'Flour, Milk, Eggs',
+            instructions: 'Mix ingredients and cook on a pan.',
+          },
+          {
+            id: 3,
+            title: 'Chocolate Cake',
+            ingredients: 'Flour, Cocoa Powder, Sugar',
+            instructions: 'Mix ingredients and bake.',
+          },
+        ],
   }),
 
   getters: {
@@ -29,15 +33,23 @@ export const useRecipesStore = defineStore('recipes', {
   },
 
   actions: {
+    saveRecipes() {
+      localStorage.setItem('recipes', JSON.stringify(this.recipes))
+    },
+
     addRecipe(recipe) {
       this.recipes.push({
         id: Date.now(),
         ...recipe,
       })
+
+      this.saveRecipes()
     },
 
     deleteRecipe(id) {
       this.recipes = this.recipes.filter((recipe) => recipe.id !== id)
+
+      this.saveRecipes()
     },
   },
 })
